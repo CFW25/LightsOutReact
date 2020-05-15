61d5699befa308fb49d2ec91e85864400e1855b7
CraigWhittington <cfwhit@hotmail.com>
2020-05-15 13:07:23 +0100

    Updates

---
+++ precache-manifest.7d2f284a498ff69a7ed6f94d38c2d75f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efc9269ec894428e665e",
+    "url": "/LightsOutReact/static/css/main.98483b6c.chunk.css"
+  },
+  {
+    "revision": "efc9269ec894428e665e",
+    "url": "/LightsOutReact/static/js/main.efc9269e.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/LightsOutReact/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "d7370cc6845256735230",
+    "url": "/LightsOutReact/static/js/runtime~main.d7370cc6.js"
+  },
+  {
+    "revision": "818dba867c59f45e5bbe8cb55ea3e2b8",
+    "url": "/LightsOutReact/index.html"
+  }
+];
